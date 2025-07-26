a3db8b54ec1893a4fedb3a52d87e0bf177771f05
St Manía <piterfreik@gmail.com>
2025-07-26 11:46:57 -0500

    Update propietario(a)-reunion.js

--- plugins/propietario(a)-reunion.js
+++ plugins/propietario(a)-reunion.js
@@ -1,9 +1,9 @@
 let handler = async(m, { conn, command }) => {
 
-let text = `*🥏 EL OWNER @${m.sender.split`@`[0]} HA SOLICITADO SU PRESENCIA EN EL STAFF, FAVOR DE DIRIGIRSE LO ANTES POSIBLE AL STAFF*
+let text = `*🥏 EL OWNER O PROPIETARIO @${m.sender.split`@`[0]} HA SOLICITADO SU PRESENCIA EN EL STAFF, FAVOR DE DIRIGIRSE LO ANTES POSIBLE AL STAFF*
 
-*𝙎𝙩𝙖𝙛𝙛 𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 🐱*`
-m.reply('*⚠️ ENVIANDO MENSAJE A TODOS LOS OWNERS*')
+*𝙎𝙩𝙖𝙛𝙛 𝘽𝙪𝙗𝙪𝘽𝙤𝙩 🐱*`
+m.reply('*⚠️ ENVIANDO MENSAJE A TODOS LOS OWNERS O PROPIETARIOS*')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
